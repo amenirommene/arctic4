cbaa0f5ac151055806a883f45e3adea6baacdd67
amenir88 <ameni.rommene@esprit.tn>
2025-02-25 09:14:10 +0100

    suiteform-with bootstrap

--- src/app/core/add-apartment/add-apartment.component.ts
+++ src/app/core/add-apartment/add-apartment.component.ts
@@ -15,18 +15,32 @@ export class AddApartmentComponent {
   ngOnInit(){
   this.myForm = new FormGroup({
     floorNB : new FormControl("", [Validators.required, Validators.pattern("[0-9]*")]),
-    apartmentNB: new FormControl("", [Validators.required, Validators.pattern("[0-9]*")])
+    apartmentNB: new FormControl("", [Validators.required, Validators.pattern("[0-9]*")]),
+    surface : new FormControl("", Validators.required),
+      terrace : new FormControl("", Validators.required),
+      surfaceTerrace : new FormControl("", Validators.required),
+      category : new FormControl("", Validators.required),
+      description : new FormControl("", [Validators.required, Validators.minLength(10)]),
+      residence : new FormControl(),
 
   })
   this.myForm2= new FormGroup({});
   }
   //getter pour retourner le FormControl floorNb
-  get floor(){
+  get floorNum(){
     return this.myForm.get('floorNB');
   }
-
+  get apartNum(){
+    return this.myForm.get('apartmentNB');
+  }
   add(){
     console.log(this.myForm.value);
   }
+  get surface() { return this.myForm.get('surface'); }
+  get surfaceTerrace() { return this.myForm.get('surfaceTerrace'); }
+  get terrace() { return this.myForm.get('terrace'); }
+  get category() { return this.myForm.get('category'); }
+  get description() { return this.myForm.get('description'); }
+  get residence() { return this.myForm.get('residence'); }
 
 }
